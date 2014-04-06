4d4e56dd3db38dc8e8d3e3d44244bd6e13e5be12
Mario Garcia <mario.ggar@gmail.com>
2014-04-06 19:26:17 +0200

    Using let expressions

--- learnyouahaskell.hs
+++ learnyouahaskell.hs
@@ -87,3 +87,14 @@ initialsForFirstName firstName = [f] ++ "."
 initialsForManyPeople :: [String] -> [String]
 initialsForManyPeople xs = [initialsForFirstName x | x <- xs] 
 
+-- LET IT BE
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^2
+    in sideArea + 2 * topArea
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+
