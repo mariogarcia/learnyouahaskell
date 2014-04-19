5fbf33f935ec06b2bca7362808b8bec17efec567
Mario Garcia <mario.ggar@gmail.com>
2014-04-19 17:49:06 +0200

    dding high order function samples

--- higherfunctions.hs
+++ higherfunctions.hs
@@ -1,2 +1,8 @@
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+mymax :: (Ord a) => a -> a -> a
+mymax a b = max a b
+
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
