9d73aed639cc11016face45c0a1a67d97d02e3a2
Mario Garcia <mario.ggar@gmail.com>
2014-08-20 15:58:13 +0200

    Adding IO examples

--- IOFirstSample.hs
+++ IOFirstSample.hs
@@ -1,4 +1,15 @@
+import Data.Char
+
 main = do
     putStrLn "Hello, What is your name?"
     name <- getLine
     putStrLn ("Hey" ++ name ++ ", you rock!!")
+
+notmain = do
+    putStrLn "Tell me your age"
+    age <- getLine
+    putStrLn "And now your name"
+    name <- getLine
+    let upperName = map toUpper name 
+    let message = "Well " ++ name ++ " although you are " ++ age ++ " you are not that bad"
+    putStrLn message
--- baby.hs
+++ baby.hs
@@ -26,3 +26,30 @@ add_vectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 middleValue :: (Int, Int, Int) -> Int
 middleValue (_, x, _) = x
 
+head' :: [a] -> a
+head' [] = error "error"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "This list only has one item: " ++ show x
+tell (x:y:[]) = "The list only has two items: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list has more than two items"
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string"
+firstLetter all@(x:xs) = "First letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: Double -> String
+bmiTell bmi
+    | bmi <= 18 = "A"
+    | bmi <= 20 = "B"
+    | bmi <= 50 = "C"
+    | otherwise = "D"
+
+letitbe :: Int -> Int
+letitbe x = 
+    let a = 2 
+        b = 3
+    in x + a + b
+
